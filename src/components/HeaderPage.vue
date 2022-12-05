222868bdd337b09030998a55a5a1150781877e5d
nicolabzz <nbuzzigoli@gmail.com>
2022-12-05 21:06:19 +0100

    Header Draft

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,5 +1,65 @@
 <template>
+<!--
+    The header is divided in 2 sections, one above the other, the "header-top" and the "header-bottom"
+-->
+<header>
 
+<!-- This div sets the max with -->
+<div class="container-header">
+    
+    <!-- Top section -->
+    <section class="header-top">
+        
+        <!-- Logo -->
+        <div class="logo">
+            <img src="" alt="">
+        </div>
+        
+        <!-- Nav options -->
+        <div class="nav-section">
+            <nav>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </nav>
+            
+            <!-- Account -->
+            <div class="account">
+            </div>
+            
+            <!-- Search bar -->
+            <div class="search-section">
+                
+            </div>
+        </div>
+    </section>
+    <!-- end/ Top section -->
+
+    <!-- Bottom section -->
+    <section class="header-bottom">
+        
+        <!-- Quote text -->
+        <div class="text-presentation">
+            
+        </div>
+        
+        <!-- Big Pic -->
+        <div class="big-pic-presentation">
+            
+        </div>
+        
+        <!-- Clients Logos -->
+        <div class="clinents-logos">
+            
+        </div>
+    </section>
+    <!-- end/ Bottom section -->
+
+</div>
+</header>
 </template>
 
 <script>
