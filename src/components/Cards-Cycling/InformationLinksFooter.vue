57d7eb36868df2af963c8bacd3f9347ed80ae63a
nicolabzz <nbuzzigoli@gmail.com>
2022-12-14 15:25:35 +0100

    Footer Information Bug Fix

--- src/components/Cards-Cycling/InformationLinksFooter.vue
+++ src/components/Cards-Cycling/InformationLinksFooter.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="information">
-		<h3>{{ footerData.informationLinks.linkTitle }}</h3>
+		<h3>{{ footerData.informationCol.linkTitle }}</h3>
 		<div id="col3">
 			<a href="#" v-for="link in footerData.informationCol.links" :key="link" >
 				{{link}}
@@ -22,12 +22,12 @@ export default {
 @import "../../css/global.scss";
 
 .information {
-	border: 2px solid violet;
+	// border: 2px solid violet;
 	width: 50%;
 	padding-top: 0.7rem;
 
 	#col3 {
-		border: 1px solid black;
+		// border: 1px solid black;
 		line-height: 3rem;
 		width: 50%;
 
--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -46,16 +46,6 @@
 					<ExploreLinksFooter :footerData="footerData" />
 
 					<InformationLinksFooter :footerData="footerData" />
-
-					<div class="information">
-						<h3>Information</h3>
-						<div id="col3">
-							<a href="#">Membership</a>
-							<a href="#">Purchase guide</a>
-							<a href="#">Privacy policy</a>
-							<a href="#">Terms of services</a>
-						</div>
-					</div>
 				</div>
 
 				<!-- Rights Reserved -->
@@ -270,34 +260,6 @@ footer {
 		height: 250px;
 		width: 50%;
 		display: flex;
-
-		.information {
-			// border: 2px solid violet;
-			width: 50%;
-			padding-top: 0.7rem;
-
-			#col3 {
-				// border: 1px solid black;
-				line-height: 3rem;
-				width: 50%;
-
-				a {
-					display: block;
-					color: #696969;
-					font-weight: 600;
-					font-size: 13px;
-					transition: 0.15s ease-in-out;
-
-					&:hover {
-						color: #20ad96;
-					}
-				}
-
-				h3 {
-					color: #333333;
-				}
-			}
-		}
 	}
 
 	.right-reserved {
